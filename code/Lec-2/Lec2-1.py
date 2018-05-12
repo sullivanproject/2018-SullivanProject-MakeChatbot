9550c7fbb129decd65c8c6dd31275662b511a1b3
Xsolute Lim <Xsolute01@gmail.com>
2018-05-13 01:04:08 +0900

    ADD: Add Lec2 Code

---
+++ code/Lec-2/Lec2-1.py
@@ -0,0 +1,8 @@
+integer = 10 # int
+string = 'sullivan' # string
+
+print(integer)
+print(string)
+
+foo = input()
+print(foo)
---
+++ code/Lec-2/Lec2-2.py
@@ -0,0 +1,8 @@
+input = input("입력해주세요! : ")
+
+if input == '안녕':
+    print('안녕하세요~!')
+elif input == '사랑해':
+    print('저도요~~')
+else:
+    print('반가워요~')
